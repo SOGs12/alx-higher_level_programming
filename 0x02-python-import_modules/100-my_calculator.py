1ac7f5f448240a25d3b9e7190d43cd5d851de244
SOGs12 <mosesoyewumi05@gmail.com>
2023-09-07 09:26:15 -0700

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
+    """Perform basic arithmetic operations."""
+
+    from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
+    # Check for the correct number of command-line arguments
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+
+    operator = sys.argv[2]
+
+    # Check if the operator is valid
+    if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    try:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+    except ValueError:
+        print("Error: Both <a> and <b> must be integers.")
+        sys.exit(1)
+
+    result = ops[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
