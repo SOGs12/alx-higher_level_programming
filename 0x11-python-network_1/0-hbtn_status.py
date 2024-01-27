6bffb7a5854d60c492a297e1c0fe4e9411014bc1
SOGs12 <mosesoyewumi05@gmail.com>
2024-01-26 17:51:34 -0800

    What's my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
     print('Body response:')
     print('\t- type: {}'.format(type(content)))
     print('\t- content: {}'.format(content))
-    utf8_content = content.decode('utf-8')
-    print('\t- utf8 content: {}'.format(utf8_content))
+    print('\t- utf8 content: {}'.format(str(content)[2:-1]))
